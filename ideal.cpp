4a70e17e2d51b45516e46c51c7d1bd0cc72072f7
sasha_oiya_ssanina <141181890+SashaOiya@users.noreply.github.com>
2024-09-23 18:42:32 +0300

    Vso

--- ideal.cpp
+++ ideal.cpp
@@ -25,8 +25,8 @@ struct list_data_s {
 struct cache_s {
     std::vector<int> data_vector= {};
     std::list<list_data_s *> cache_list = {};
-    std::unordered_map< int, list_data_s *> data_hash_table  = {};
-    std::unordered_map< int, list_data_s *> cache_hash_table  = {};
+    std::unordered_multimap< int, list_data_s *> data_hash_table  = {};
+    std::unordered_multimap< int, list_data_s *> cache_hash_table = {};
           // irr, 
     std::multimap<int, list_data_s *> cache_map = {};
     std::hash<int> hash_int;
@@ -36,12 +36,9 @@ struct cache_s {
 
 void Print_Vector ( std::vector<int> &data );
 void Ctor ( cache_s *cache );
-int Filling_Cache ( cache_s *cache );
-
-bool compare(const std::pair<int, int>&a, const std::pair<int, int>&b)
-{
-   return a.first<b.first;
-}
+size_t Filling_Cache ( cache_s *cache );
+bool compare(const std::pair<int, list_data_s *>&a, const std::pair<int, list_data_s *>&b);
+int Cache_Processing ( cache_s *cache );
 
 int main () 
 {
@@ -49,15 +46,30 @@ int main ()
 
     Ctor ( &cache );
 
-    Filling_Cache ( &cache );
+    std::cout << Cache_Processing ( &cache );
 
-    /*for (const auto& p : cache.cache_map )
-        std::cout << p.first <<'\n'; */
+    /*Filling_Cache ( &cache );
+
+    for (const auto& p : cache.cache_hash_table)
+        std::cout << p.first << " => " << p.second->current_irr << '\n';
+
+    std::cout << (max_element(cache.cache_hash_table.begin(), cache.cache_hash_table.end(), compare)->second)->data; */
+
+    /*std::multimap<int, int> x = {};
+    x.emplace ( 1, 2);
+    x.emplace ( 1, 3);
+    x.emplace ( 2, 3);
+    x.emplace ( 3, 5);
+
+    for (const auto& p : x)
+        std::cout << p.first << " => " << p.second << '\n';
+
+    std::cout << max_element(x.begin(), x.end(), compare)->second; */
 
     return 0;
 }
 
-list_data_s *Find_Elem_Hash_Table ( cache_s *cache, std::unordered_map< int, list_data_s *> hash_table, int desired_elem );
+list_data_s *Find_Elem_Hash_Table ( cache_s *cache, std::unordered_multimap< int, list_data_s *> hash_table, int desired_elem );
 
 void Ctor ( cache_s *cache ) 
 {
@@ -78,7 +90,7 @@ $
             assert ( element_pointer != nullptr );
             element_pointer->data = new_element;
 $
-            cache->data_hash_table.try_emplace ( element_pointer->data, element_pointer );
+            cache->data_hash_table.emplace ( element_pointer->data, element_pointer );
         }
 $
         element_pointer->cells.push_back ( i );
@@ -88,13 +100,13 @@ $
     // error
 }
 
-list_data_s *Find_Elem_Hash_Table ( cache_s *cache, std::unordered_map< int, list_data_s *> hash_table, int desired_elem )
+list_data_s *Find_Elem_Hash_Table ( cache_s *cache, std::unordered_multimap< int, list_data_s *> hash_table, int desired_elem )
 {
     assert ( cache != nullptr );
 
     const int hash = cache->hash_int(desired_elem); // int
 
-    std::unordered_map<int, list_data_s *>::iterator elem_iterator = hash_table.find ( hash ); // int
+    std::unordered_multimap<int, list_data_s *>::iterator elem_iterator = hash_table.find ( hash ); // int
     if ( elem_iterator == hash_table.end () ) {
         #ifdef DEBUG 
             std::cout << "No founded\n"; //
@@ -106,32 +118,59 @@ $
     return elem_iterator->second;
 } 
 
-int Filling_Cache ( cache_s *cache );
+size_t Filling_Cache ( cache_s *cache );
 
-void Cache_Processing ( cache_s *cache )
+bool compare(const std::pair<int, list_data_s *>&a, const std::pair<int, list_data_s *>&b)
+{
+   return a.second->current_irr < b.second->current_irr;
+}
+
+int Cache_Processing ( cache_s *cache )
 {
     assert ( cache != nullptr );
 
     // add first data in cache_list
-    Filling_Cache ( cache ); // return value
+    size_t misses_total_n = Filling_Cache ( cache ); 
 
     size_t elem_n = cache->data_vector.size();
     for ( int i = 0; i < elem_n; ++i ) {
+        // get next elem
+        int element = cache->data_vector.front(); 
+        cache->data_vector.erase ( cache->data_vector.begin() );
 
+        // count its irr
+        list_data_s *element_pointer = Find_Elem_Hash_Table ( cache, cache->data_hash_table, element ); 
+        assert ( element_pointer != nullptr );
+        size_t element_irr=(element_pointer->cells.size()>1) ? element_pointer->cells[1]-element_pointer->cells[0]-1 : element_irr=cache->data_vector.size()+1; 
+        element_pointer->cells.erase( element_pointer->cells.begin() );
+        element_pointer->current_irr = element_irr;
+
+        // если элемент не найден, то прибавляем к переменной 
+        if ( Find_Elem_Hash_Table ( cache, cache->cache_hash_table, element ) == nullptr ) {
+            // search max irr
+            list_data_s * max_element_pointer = max_element(cache->cache_hash_table.begin(), cache->cache_hash_table.end(), compare)->second; 
+            // delete its
+            cache->cache_list.remove( max_element_pointer);
+            cache->cache_hash_table.erase( cache->cache_hash_table.find ( max_element_pointer->data ) );
+            // assert ( Find_Elem_Hash_Table ( ...) != nullptr )
+
+            // insert in cache_list one
+            cache->cache_list.push_front ( element_pointer );
+            cache->cache_hash_table.emplace ( element_pointer->data, element_pointer );
+            // assert ( Find_Elem_Hash_Table ( ...) != nullptr )
+            ++misses_total_n;
+        }
     }
-    
-    // processing ( the )
-    // search the biggest 
 
     // errors
+    return misses_total_n;
 }
 
-int Filling_Cache ( cache_s *cache ) //pair
+size_t Filling_Cache ( cache_s *cache ) //pair
 {
     assert ( cache != nullptr );
 
-    //size_t max_irr = 0;
-
+    size_t misses_number = 0;
     for ( int i = 0; i < cache->capacity; ++i ) {
         // get element 
         int element = cache->data_vector.front(); 
@@ -140,39 +179,22 @@ int Filling_Cache ( cache_s *cache ) //pair
         // get irr
         list_data_s *element_pointer = Find_Elem_Hash_Table ( cache, cache->data_hash_table, element ); 
         assert ( element_pointer != nullptr );
-        size_t element_irr = 0;
-        if ( element_pointer->cells.size() > 1 ) {
-            element_irr = element_pointer->cells[1] - element_pointer->cells[0] - 1;
-        }
-        else {
-            element_irr = cache->data_vector.size() + 1;
-        }
+        size_t element_irr=(element_pointer->cells.size()>1) ? element_pointer->cells[1]-element_pointer->cells[0]-1 : element_irr=cache->data_vector.size()+1; 
         element_pointer->cells.erase( element_pointer->cells.begin() );
 
         // filling cache ( list, hash table, map )
         if ( Find_Elem_Hash_Table ( cache, cache->cache_hash_table, element ) == nullptr ) {
             cache->cache_list.push_front ( element_pointer );
-            cache->cache_hash_table.try_emplace ( element_pointer->data, element_pointer );
+            cache->cache_hash_table.emplace ( element_pointer->data, element_pointer );
             // assert ( Find_Elem_Hash_Table ( ...) != nullptr )
-            cache->cache_map.emplace ( element_irr, element_pointer );
+            ++misses_number;
         }
         else { --i; }
         element_pointer->current_irr = element_irr;
-
-        // search the biggest 
-        //max_irr = ( element_irr > max_irr ) ? element_irr : max_irr;
     }
 
-    return 0;
+    return misses_number;
 }
-
-int Processing ( cache_s *cache )
-{
-    assert ( cache != nullptr );
-
-
-    return 0;//
-}  
 
 void Print_Vector ( std::vector<int> &data )
 {
