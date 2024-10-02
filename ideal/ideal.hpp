cdc882a54a0e8692c7a9942ab7f8f2ac34b3c020
sasha_oiya_ssanina <141181890+SashaOiya@users.noreply.github.com>
2024-10-03 01:32:25 +0300

    Debug

--- ideal/ideal.hpp
+++ ideal/ideal.hpp
@@ -10,7 +10,6 @@
 #include <fstream>
 #include <ctime>
 #include <deque>
-#include <list>
 
 #define NDEBUG
 
@@ -28,20 +27,15 @@ enum errors {
 
 template <typename T, typename U>
 class Cache_c {
-    struct list_data_s {
-        T data = 0; 
-        size_t irr = 0;
-        typename std::list<T>::iterator cache_iterator = {};
-    };
 private :
-    std::deque<T> data_vector;
+    std::deque<T> data_vector = {};
                             // data, cells
-    std::unordered_map< T, std::deque<int>> data_hash_table  = {};
+    std::unordered_map< T, std::deque<int> > data_hash_table  = {};
     std::hash<T> hash_func;
-            // T may be
-    std::list< T > cache_list = {}; // name
+
+    std::vector<T> cache_list = {}; // name
                       // data, irr
-    std::unordered_map< T, list_data_s > cache_hash_table = {};
+    std::unordered_map< T, int> cache_hash_table = {}; 
 
     size_t capacity = 0; // a_
     size_t elements_number = 0;
@@ -58,15 +52,16 @@ $
         for ( size_t i = 0, elem_number = elements_number; i < elem_number; ++i ) {
             T new_element = 0;
             std::cin >> new_element;
-            std::deque<int> *element_cells_pointer = find_elem_hash_table ( new_element );
+            std::deque<int> *element_pointer = find_elem_hash_table ( new_element );
 
-            if ( element_cells_pointer == nullptr ) {
+            if ( element_pointer == nullptr ) {
                 std::deque<int> cell = {}; 
 $               data_hash_table.emplace ( new_element, cell );
-                element_cells_pointer = find_elem_hash_table ( new_element );
+                element_pointer = find_elem_hash_table ( new_element );
             }
-            assert ( element_cells_pointer != nullptr);
-$           element_cells_pointer->push_back ( i );
+            assert ( element_pointer != nullptr);
+
+$           element_pointer->push_back ( i );
             data_vector.push_back ( new_element );
         }
 
@@ -98,70 +93,74 @@ $
 
     int cache_processing ()
     {
-        size_t misses_total_n = filling_cache (); 
+        size_t misses_total_n = 0;//= filling_cache (); 
 $
         size_t elem_n = data_vector.size();
-        for ( size_t i = 0; i < elem_n; ++i ) {
+        for ( size_t i = 0; ( i < elem_n ) && ( data_vector.size() ); ++i ) {
             // get next elem
             T element = data_vector.front();  // list may be
-            data_vector.pop_front ();
+            data_vector.pop_front();
 $
             // get irr
-            std::deque<int> *element_cells_pointer = find_elem_hash_table ( element ); 
-            assert ( element_cells_pointer != nullptr );
-            list_data_s *irr = find_irr_hash_table (element );
-            assert ( element_cells_pointer->size() >= 1 );
-            if ( ( element_cells_pointer->size() == 1 ) && ( irr == nullptr ) ) {
-                element_cells_pointer->pop_front();
+            std::deque<int> *element_vector_pointer = find_elem_hash_table ( element ); 
+            assert ( element_vector_pointer != nullptr );
+            int irr = find_irr_hash_table (element );
+            assert ( element_vector_pointer->size() >= 1 );
+            if ( ( element_vector_pointer->size() == 1 ) && ( irr == -1 ) ) {
+                element_vector_pointer->pop_front();
                 data_hash_table.erase ( data_hash_table.find ( element ) ); 
                 
                 ++misses_total_n;
 
                 continue;
             }
-            size_t element_irr = (*element_cells_pointer)[1] - (*element_cells_pointer)[0] - 1; 
-            element_cells_pointer->pop_front(); 
+            size_t element_irr = (*element_vector_pointer)[1] - (*element_vector_pointer)[0] - 1; //
+            element_vector_pointer->pop_front(); //
 
-            if ( irr == nullptr ) {
+            if ( ( irr == -1 ) && ( cache_list.size() < capacity ) ) { 
+$               cache_list.push_back ( element );
+                cache_hash_table.emplace ( element, element_irr );
+                assert ( find_irr_hash_table ( element ) != -1 );
+
+                ++misses_total_n;
+            }
+            else if ( irr == -1 ) {
                 // search max irr
-                T max_irr_element = max_element ( cache_hash_table.begin(), cache_hash_table.end(), compare)->first;  // ---
-                list_data_s *max_irr = find_irr_hash_table ( max_irr_element );
-                assert ( max_irr != nullptr );
-                if ( max_irr->irr < element_irr ) {
+$               T max_irr_element = max_element ( cache_hash_table.begin(), cache_hash_table.end(), compare)->first; 
+$               size_t max_irr = find_irr_hash_table ( max_irr_element );
+                if ( max_irr < element_irr ) {
                     ++misses_total_n;
                     continue;
                 }
                 // delete its
-                cache_list.erase ( max_irr->cache_iterator ); // ---
+$               cache_list.erase ( std::remove( cache_list.begin(), cache_list.end(), max_irr_element ), cache_list.end() );
                 cache_hash_table.erase( cache_hash_table.find ( max_irr_element ) ); 
-                assert ( find_irr_hash_table ( element ) == nullptr ); //
+                assert ( find_irr_hash_table ( element ) == -1 ); //
 $
                 // insert in cache_list one
                 cache_list.push_back ( element );
-                // irr
-                list_data_s data = { element, element_irr, --(cache_list.end()) };
-                cache_hash_table.emplace ( element, data );
-                assert ( find_irr_hash_table ( element ) != nullptr );
+                cache_hash_table.emplace ( element, element_irr );
+                assert ( find_irr_hash_table ( element ) != -1 );
 
                 ++misses_total_n;
             }
             else {
-                cache_hash_table[element].irr = element_irr;
+                cache_hash_table[element] = element_irr;
             }
         }
 
         return elements_number - misses_total_n;
     }
     
-    static bool compare ( const std::pair<T, list_data_s> &a, const std::pair<T, list_data_s> &b )
+    static bool compare ( const std::pair<T, int> &a, const std::pair<T, int> &b )
     {
-        return a.second.irr < b.second.irr;
+        return a.second < b.second;
     }
 
 private:
     std::deque<int> *find_elem_hash_table ( T desired_elem )
     {
-        const T hash = hash_func ( desired_elem );  ////
+        const T hash = hash_func ( desired_elem ); 
         typename std::unordered_map< T, std::deque<int>>::iterator elem_iterator = data_hash_table.find ( hash ); 
         if ( elem_iterator == data_hash_table.end () ) {
 
@@ -171,17 +170,17 @@ $
         return &(elem_iterator->second);
     } 
 
-    list_data_s *find_irr_hash_table ( const T& desired_elem )  // const &
+    inline int find_irr_hash_table ( T desired_elem )
     {
         const T hash = hash_func ( desired_elem ); 
 
-        typename std::unordered_map< T, list_data_s>::iterator elem_iterator = cache_hash_table.find ( hash ); 
+        typename std::unordered_map< T, int>::iterator elem_iterator = cache_hash_table.find ( hash ); 
         if ( elem_iterator == cache_hash_table.end () ) {
 
-            return nullptr; 
+            return -1; 
         }
 $
-        return &(elem_iterator->second);
+        return elem_iterator->second;
     } 
 
     inline size_t filling_cache () 
@@ -190,36 +189,35 @@ $
 
         for ( size_t i = 0; ( i < capacity ) && ( data_vector.size()); ++i ) { 
             // get element 
-            T& element = data_vector.front();   ///////
+            T element = data_vector.front();  
             data_vector.pop_front();
 
             // get irr
-            std::deque<int> *element_cells_pointer = find_elem_hash_table ( element ); 
-            list_data_s *element_irr = find_irr_hash_table ( element );
-            assert ( element_cells_pointer != nullptr ); // in find
-            assert ( !(element_cells_pointer->empty()) );
-            if ( ( element_cells_pointer->size() == 1 ) && ( element_irr == nullptr ) ) {
-                element_cells_pointer->pop_front();
+            std::deque<int> *element_pointer = find_elem_hash_table ( element ); 
+            int element_irr = find_irr_hash_table ( element );
+            assert ( element_pointer != nullptr );
+            assert ( element_pointer->size() >= 1 );
+            if ( ( element_pointer->size() == 1 ) && ( element_irr == -1 ) ) {
+                element_pointer->pop_front();
                 data_hash_table.erase ( data_hash_table.find ( element ) );
                 ++misses_number; 
                 --i;
 $
                 continue;
             }
-            size_t new_element_irr = (*element_cells_pointer)[1] - (*element_cells_pointer)[0] - 1; 
-            element_cells_pointer->pop_front(); 
+            size_t new_element_irr = (*element_pointer)[1] - (*element_pointer)[0] - 1; 
+            element_pointer->pop_front(); 
 $
             // filling cache 
-            if ( element_irr == nullptr ) { 
+            if ( element_irr == -1 ) { 
                 cache_list.push_back ( element );
-                list_data_s data = { element, new_element_irr, --(cache_list.end()) };
-                cache_hash_table.emplace ( element, data );
-                assert ( find_irr_hash_table ( element ) != nullptr );
+                cache_hash_table.emplace ( element, new_element_irr );
+                assert ( find_irr_hash_table ( element ) != -1 );
 
                 ++misses_number;
             }
             else { 
-                cache_hash_table[element].irr = new_element_irr;
+                cache_hash_table[element] = new_element_irr;
                 --i;
             }
         }
