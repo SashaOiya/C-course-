ac2e7592948fabff541e2ecbbe87005e550cc655
sasha_oiya_ssanina <141181890+SashaOiya@users.noreply.github.com>
2024-10-12 21:17:18 +0300

    Unworking

--- ideal/ideal.hpp
+++ ideal/ideal.hpp
@@ -10,95 +10,84 @@
 #include <vector>
 #include <list>
 
+// name private members as m_field or field_
+
+// + slow_get_page
+// + lookup_update
+
 template <typename T, typename U>
 class Cache_c {
-    std::deque<T> data_vector = {};
+    std::deque<T> data_vector;
                             // data, cells
-    std::unordered_map< T, std::deque<int> > data_hash_table  = {};
-    std::vector<T> cache_storage = {}; 
+    std::unordered_map< T, std::deque<int> > data_hash_table;
+    std::vector<T> cache_storage;     
                       // data, irr
-    std::unordered_map< T, int> cache_hash_table = {}; 
+    std::unordered_map< T, int> cache_hash_table;
 
     size_t capacity = 0; // a_
     size_t elements_number = 0;
-    std::hash<T> hash_func;
 
 public :
-    Cache_c ( size_t capacity, size_t elements_number ) : capacity( capacity ), elements_number ( elements_number ) {};
+    // template<typename It> // typename -> std::input_iterator
+    Cache_c ( size_t capacity, size_t elements_number/*, It from, It to*/)
+        : capacity( capacity ), elements_number ( elements_number )/*, data_vector(from, to)*/ {}
 
-    void reading_data () 
+    void reading_data () // divide into a part that reads data from stdin and a part that changes the state of the cache
     {
         data_hash_table.reserve ( elements_number );
-        assert ( cache_storage.max_size() > capacity );
 
         for ( size_t i = 0; i < elements_number; ++i ) {
             T new_element = 0;
             std::cin >> new_element;
-            std::deque<int> *element_pointer = search_in_data_hash_table ( new_element );
-
-            if ( element_pointer == nullptr ) {
-                std::deque<int> cell = {}; 
-                data_hash_table.emplace ( new_element, cell );
-                element_pointer = search_in_data_hash_table ( new_element );
-            }
-            assert ( element_pointer != nullptr);
-
-            element_pointer->push_back ( i );
             data_vector.push_back ( new_element );
+
+            data_hash_table[new_element].push_back(i);
         }
     }
 
-    void output ( const int output ) 
-    {
-        std::cout << output << '\n';
-    }
+    const int MAX_IRR = elements_number + 1; // remove
 
-    const int MAX_IRR = elements_number + 1;
-
-    int cache_processing ()
+    int cache_processing () // lookup_update in loop, loop in main
     {
         size_t misses_total_n = 0;
 
-        for ( size_t i = 0, elem_n = data_vector.size(); ( i < elem_n ) && ( data_vector.size() ); ++i ) {
+        for ( auto itt = data_vector.begin(); itt != data_vector.end(); ++itt ) {
             // get next elem
-            T element = data_vector.front(); 
+            T element = std::move(*itt);
             data_vector.pop_front();
 
             // get irr
-            std::deque<int> *element_vector_pointer = search_in_data_hash_table ( element ); 
-            auto irr = search_in_cache_hash_table (element );
-            size_t element_recency = 0; //
+            auto irr = cache_hash_table.find (element );
             if ( irr == cache_hash_table.end() ) { ++misses_total_n; }
+            auto &element_vector = data_hash_table[element];
 
-            if (  ( element_vector_pointer->size() == 1 ) && ( irr == cache_hash_table.end() ) ) {
-                element_vector_pointer->pop_front();
-                data_hash_table.erase ( data_hash_table.find ( element ) ); 
+            if ( element_vector.size() == 1 && irr == cache_hash_table.end() ) {
+                element_vector.pop_front();
+                data_hash_table.erase ( element );
 
                 continue;
             }
-            element_recency = ( element_vector_pointer->size() == 1 ) ? MAX_IRR:(*element_vector_pointer)[1] - (*element_vector_pointer)[0] - 1; 
-            element_vector_pointer->pop_front();
 
-            if ( ( irr == cache_hash_table.end() ) && ( cache_storage.size() < capacity ) ) { 
-                cache_storage.push_back ( element );
-                cache_hash_table.emplace ( element, element_recency );
-                assert ( search_in_cache_hash_table ( element ) != cache_hash_table.end() );
-            }
-            else if ( irr == cache_hash_table.end() ) {
-                T max_irr_element = get_element_with_max_recency ();
-                size_t max_recency = search_in_cache_hash_table( max_irr_element )->second;
-                if ( ( max_recency <= element_recency ) ) {
-                    continue;
+            size_t element_recency = ( element_vector.size() == 1 ) ? MAX_IRR : element_vector[1] - element_vector[0] - 1;
+            element_vector.pop_front();
+
+            if (irr == cache_hash_table.end()) {
+                if (cache_storage.size() < capacity) {
+                    cache_storage.push_back ( element );
+                    cache_hash_table.emplace ( element, element_recency );
+                    //assert ( cache_hash_table.contains ( element ) );
+                } else {
+                    auto [max_irr_it, max_recency] = get_element_with_max_recency ();
+                    if ( ( max_recency <= element_recency ) ) {
+                        continue;
+                    }
+                    cache_storage.erase ( std::remove( cache_storage.begin(), cache_storage.end(), *max_irr_it ),
+                                          cache_storage.end() );
+                    cache_hash_table.erase( *max_irr_it );
+                    cache_storage.push_back ( element );
+                    cache_hash_table.emplace ( element, element_recency );
                 }
-                // delete its
-                cache_storage.erase ( std::remove( cache_storage.begin(), cache_storage.end(), max_irr_element ), cache_storage.end() );
-                cache_hash_table.erase( cache_hash_table.find ( max_irr_element ) ); 
-
-                // insert in cache_list one
-                cache_storage.push_back ( element );
-                cache_hash_table.emplace ( element, element_recency );
-            }
-            else {
+            } else {
                 cache_hash_table[element] = element_recency;
             }
         }
@@ -107,39 +96,22 @@ public :
     }
 
 private:
-
-    std::deque<int> *search_in_data_hash_table ( T desired_elem )
-    {
-        typename std::unordered_map< T, std::deque<int>>::iterator elem_iterator = data_hash_table.find ( desired_elem ); 
-        if ( elem_iterator == data_hash_table.end () ) { return nullptr; }
-
-        return &(elem_iterator->second);
-    } 
-
-    auto search_in_cache_hash_table ( T desired_elem )
-    {
-        typename std::unordered_map< T, int>::iterator elem_iterator = cache_hash_table.find ( desired_elem ); 
- 
-        return elem_iterator;
-    } 
-
-    T get_element_with_max_recency () 
+    auto get_element_with_max_recency ()
     {
         int max_recency = 0;
-        T max_recency_element = 0;
-        typename std::vector<T>::iterator element_iterator = cache_storage.begin();
+        auto it = cache_storage.begin();
+        auto max_recency_it = it;
 
-        for ( size_t i = 0, size = cache_storage.size(); i < size; ++i ) {
-            T current_element = *element_iterator;
-            int current_recency = search_in_cache_hash_table( current_element )->second;
+        for (auto ite = cache_storage.end(); it != ite; ++it) {
+            int current_recency = cache_hash_table[*it];
             if ( current_recency == MAX_IRR ) {
-                return current_element;
+                return std::pair{it, MAX_IRR};
             }
             else if ( current_recency > max_recency ) {
-                max_recency_element = current_element;
+                max_recency = current_recency;
+                max_recency_it = it;
             }
-            ++element_iterator;
         }
-        return max_recency_element;
+        return std::pair{max_recency_it, max_recency};
     }
 };
